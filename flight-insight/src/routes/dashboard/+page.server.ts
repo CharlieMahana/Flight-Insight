8d30ad2e9c0e01fce1508c1902d0c2df6648f303
Spaceface16518 <amrit.rathie@gmail.com>
2023-01-29 02:40:05 -0600

    i'm so tired

--- flight-insight/src/routes/dashboard/+page.server.ts
+++ flight-insight/src/routes/dashboard/+page.server.ts
@@ -1,53 +1,59 @@
-import { fail } from "@sveltejs/kit";
-import type { PageServerLoad } from "../$types";
+import { env } from "$env/dynamic/private";
+import { error } from "@sveltejs/kit";
+import { MongoClient } from "mongodb";
+import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ url, fetch, locals }) => {
-  const recordLocater = url.searchParams.get("recordLocater");
-  if (!recordLocater) return fail(404);
+  const recordLocator = url.searchParams.get("recordLocator");
+  // if (!recordLocator) return error(404, "Record not found");
 
   // load flight data from the database
-  const db = locals.db;
+  const client = await new MongoClient(env.MONGODB_URI, {}).connect();
+  const db = client.db("weather");
   // @ts-ignore
-  const flight = await db.flights.find({ recordLocater });
+  const flight = await db
+    .collection("flights")
+    .findOne({ recordLocator }, { projection: { _id: 0 } });
+  if (!flight) throw "FLight not found";
 
-  const origin = flight.origin.location;
-  const destination = flight.destination.location;
+  const origin = flight?.origin.location;
+  const destination = flight?.destination.location;
 
   // get weather data from meteo api
-  const originWeather = fetch(
+  const originWeather = await fetch(
     `https://api.open-meteo.com/v1/forecast?latitude=${origin.latitude}&longitude=${origin.longitude}&hourly=temperature_2m,precipitation,cloudcover`
-  )
-    .then((res) => res.json())
-    .then((res) => {
-      const now = new Date();
-      const min = res.hourly.time
-        .map((hour: string) => new Date(hour).getTime() - now.getTime())
-        .filter((diff: number) => diff > 0)
-        .min();
-      const index = res.hourly.time.indexOf(now.getTime() + min);
-      return {
-        temperature: res.hourly.temperature_2m[index],
-        precipitation: res.hourly.precipitation[index],
-        cloudcover: res.hourly.cloudcover[index],
-      };
-    });
-  const destinationWeather = fetch(
+  ).then((res) => res.json());
+  // .then((res) => {
+  //   const now = new Date();
+  //   const min = res.hourly.time
+  //     .map((hour: string) => new Date(hour).getTime() - now.getTime())
+  //     .filter((diff: number) => diff > 0)
+  //     .min();
+  //   const index = res.hourly.time.indexOf(now.getTime() + min);
+  //   return {
+  //     temperature: res.hourly.temperature_2m[index],
+  //     precipitation: res.hourly.precipitation[index],
+  //     cloudcover: res.hourly.cloudcover[index],
+  //   };
+  // });
+  const destinationWeather = await fetch(
     `https://api.open-meteo.com/v1/forecast?latitude=${destination.latitude}&longitude=${destination.longitude}&hourly=temperature_2m,precipitation,cloudcover`
-  )
-    .then((res) => res.json())
-    .then((res) => {
-      const now = new Date();
-      const min = res.hourly.time
-        .map((hour: string) => new Date(hour).getTime() - now.getTime())
-        .filter((diff: number) => diff > 0)
-        .min();
-      const index = res.hourly.time.indexOf(now.getTime() + min);
-      return {
-        temperature: res.hourly.temperature_2m[index],
-        precipitation: res.hourly.precipitation[index],
-        cloudcover: res.hourly.cloudcover[index],
-      };
-    });
+  ).then((res) => res.json());
+  // .then((res: any) => {
+  //   const now = new Date();
+  //   const min = res.hourly.time.map(
+  //     (hour: string | null) => new Date(hour || now).getTime() - now.getTime()
+  //   );
+  //   .filter((diff: number) => diff > 0)
+  // .min();
+  // const index = res.hourly.time.indexOf(now.getTime() + min);
+  // return {
+  //   temperature: res.hourly.temperature_2m[index],
+  //   precipitation: res.hourly.precipitation[index],
+  //   cloudcover: res.hourly.cloudcover[index],
+  // };
+  // return min;
+  // });
 
   return {
     flight,
--- flight-insight/src/routes/dashboard/+page.svelte
+++ flight-insight/src/routes/dashboard/+page.svelte
@@ -1,10 +1,11 @@
 <script>
-    import { onMount } from 'svelte'
+    import { onMount } from "svelte";
     import mapboxgl from "mapbox-gl";
-    import turf from 'turf';
-    import { time_ranges_to_array } from 'svelte/internal';
-    import AmericanIcon from '$lib/components/AmericanIcon.svelte';
-    mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhcmxpZW1haGFuYSIsImEiOiJja3FicmNhOWQwZDQwMnVvZW5pd3BnNGc4In0._TBwk5GaE5qqih2pilaLNw' // default public access token
+    import turf from "turf";
+    import { time_ranges_to_array } from "svelte/internal";
+    import AmericanIcon from "$lib/components/AmericanIcon.svelte";
+    mapboxgl.accessToken =
+        "pk.eyJ1IjoiY2hhcmxpZW1haGFuYSIsImEiOiJja3FicmNhOWQwZDQwMnVvZW5pd3BnNGc4In0._TBwk5GaE5qqih2pilaLNw"; // default public access token
 
     const departingCoordinates = [-97.0403, 32.8998];
     const arrivingCoordinates = [-71.0096, 42.3656];
@@ -19,107 +20,119 @@
             center: [-98, 38],
             zoom: 3.3,
             attributionControl: false,
-        }).addControl(new mapboxgl.AttributionControl({
-            compact: true,
-        }));     
+        }).addControl(
+            new mapboxgl.AttributionControl({
+                compact: true,
+            })
+        );
 
-        map.on('load', async () => {
-            map.addSource('line', {
-                'type': 'geojson',
-                'data': {
-                    'type': 'FeatureCollection',
-                    'features': [
+        map.on("load", async () => {
+            map.addSource("line", {
+                type: "geojson",
+                data: {
+                    type: "FeatureCollection",
+                    features: [
                         {
-                            'type': 'Feature',
-                            'geometry': {
-                                'type': 'LineString',
-                                'coordinates': [departingCoordinates, arrivingCoordinates]
-                            }
+                            type: "Feature",
+                            geometry: {
+                                type: "LineString",
+                                coordinates: [
+                                    departingCoordinates,
+                                    arrivingCoordinates,
+                                ],
+                            },
                         },
-                    ]
-                }
+                    ],
+                },
             });
 
             map.addLayer({
-                'id': 'line-animation',
-                'type': 'line',
-                'source': 'line',
-                'layout': {
-                'line-cap': 'round',
-                'line-join': 'round'
+                id: "line-animation",
+                type: "line",
+                source: "line",
+                layout: {
+                    "line-cap": "round",
+                    "line-join": "round",
                 },
-                'paint': {
-                'line-color': '#000000',
-                'line-width': 2,
-                'line-opacity': 0.8
-                }
+                paint: {
+                    "line-color": "#000000",
+                    "line-width": 2,
+                    "line-opacity": 0.8,
+                },
             });
 
             // create departing airport pin
-            const departure = document.createElement('div');
-            departure.className = 'marker';
-            new mapboxgl.Marker(departure).setLngLat(departingCoordinates).addTo(map);
+            const departure = document.createElement("div");
+            departure.className = "marker";
+            new mapboxgl.Marker(departure)
+                .setLngLat(departingCoordinates)
+                .addTo(map);
 
             // create arrival airport pin
-            const arrival = document.createElement('div');
-            arrival.className = 'marker';
-            new mapboxgl.Marker(arrival).setLngLat(arrivingCoordinates).addTo(map);
+            const arrival = document.createElement("div");
+            arrival.className = "marker";
+            new mapboxgl.Marker(arrival)
+                .setLngLat(arrivingCoordinates)
+                .addTo(map);
 
             // add weather layer
             const timeSlices = await fetch(
                 "https://api.weather.com/v3/TileServer/series/productSet/PPAcore?apiKey=2ec2232d72f1484282232d72f198421d"
             );
-            const weatherData = await timeSlices.json()
+            const weatherData = await timeSlices.json();
             const time = weatherData.seriesInfo.radar.series[0].ts;
-            map.addSource('twcRadar', {
-                type: 'raster',
+            map.addSource("twcRadar", {
+                type: "raster",
                 tiles: [
-                    'https://api.weather.com/v3/TileServer/tile/radar?ts=' + time + '&xyz={x}:{y}:{z}&apiKey=2ec2232d72f1484282232d72f198421d'
+                    "https://api.weather.com/v3/TileServer/tile/radar?ts=" +
+                        time +
+                        "&xyz={x}:{y}:{z}&apiKey=2ec2232d72f1484282232d72f198421d",
                 ],
                 tileSize: 256,
             });
-            map.addLayer(
-                {
-                    id: 'radar',
-                    type: 'raster',
-                    source: 'twcRadar',
-                    paint: {
-                        'raster-opacity': 0.7,
-                    },
+            map.addLayer({
+                id: "radar",
+                type: "raster",
+                source: "twcRadar",
+                paint: {
+                    "raster-opacity": 0.7,
                 },
-            );
+            });
         });
-    })
+    });
 </script>
 
 <svelte:head>
-    <link href="https://api.tiles.mapbox.com/mapbox-gl-js/v0.45.0/mapbox-gl.css" rel='stylesheet' />
-    <script src="https://api.mapbox.com/mapbox-assembly/v0.23.2/assembly.js"></script>
+    <link
+        href="https://api.tiles.mapbox.com/mapbox-gl-js/v0.45.0/mapbox-gl.css"
+        rel="stylesheet"
+    />
+    <script
+        src="https://api.mapbox.com/mapbox-assembly/v0.23.2/assembly.js"
+    ></script>
 </svelte:head>
 <div>
     <div class="w-full bg-primary h-16 flex ">
         <div class="flex flex-row align-middle text-center ">
             <div class="w-12 h-12 mr-4 m-auto ml-4">
-                <AmericanIcon></AmericanIcon>
+                <AmericanIcon />
             </div>
             <p class="align-middle m-auto text-base-100">Flight Insight</p>
         </div>
-        <div>
-
-        </div>
+        <div />
     </div>
     <div>
         <div>
-            <div id="map" class="w-3/4 h-64 md:max-h-[500] "></div>
+            <div id="map" class="w-3/4 h-64 md:max-h-[500] " />
             <style>
                 .marker {
-                background-image: url('https://www.iconpacks.net/icons/2/free-airport-location-icon-2959-thumb.png');
-                background-size: cover;
-                width: 50px;
-                height: 50px;
-                border-radius: 50%;
-                cursor: pointer;
-                top: -20px;
+                    background-image: url("https://www.iconpacks.net/icons/2/free-airport-location-icon-2959-thumb.png");
+                    background-size: cover;
+                    width: 50px;
+                    height: 50px;
+                    border-radius: 50%;
+                    cursor: pointer;
+                    top: -20px;
                 }
             </style>
             <div id="map" class="w-3/4 h-64 flex flex-col md:flex-row">
@@ -128,7 +141,7 @@
                     <p>{data.originWeather.temperature}</p>
                     {#if data.originWeather.precipitation > 0}
                         <img src="../../static/weather/rainy-2.svg" />
-                    {:else if data.originWeather.cloudcover >50}
+                    {:else if data.originWeather.cloudcover > 50}
                         <img src="../../static/weather/cloudy-day-2.svg" />
                     {:else}
                         <img src="../../static/weather/day.svg" />
@@ -140,7 +153,6 @@
                 </div>
             </div>
         </div>
-        
 
         <style>
             #map {
