f010c868adaae657f750ae07e514a6b694abc7c1
heartgg <a.szumski98@gmail.com>
2023-01-29 03:55:01 -0600

    mini fixes for login screen

--- flight-insight/src/routes/+page.svelte
+++ flight-insight/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <body class="bg-primary h-screen w-screen flex justify-center content-center">
   <div class="m-auto lg:w-1/2 w-10/12">
-    <p class="text-base-100 text-center md:text-4xl text-2xl">American Airlines Flight Insight</p>
-    <div class="card bg-base-100 p-2 justify-center content-center text-base-100 md:w-1/2 m-auto mt-10 md:h-128 h-96">
+    <!-- <p class="text-base-100 text-center md:text-4xl text-2xl font-roboto">American Airlines Flight Insight</p> -->
+    <div class="card bg-base-100 p-4 justify-center content-center text-base-100 md:w-96 m-auto mt-10 md:h-128">
       <div class="card-title m-auto">
         <div class="tabs tabs-boxed">
           <a class="{isSignIn ? 'SignedIn' : 'tab'}" on:click={handleSignInClick}>Sign In</a> 
@@ -9,20 +9,20 @@
         </div>
       </div>
       <div class="card-body justify center content center items-center m-auto flex">
-        <div class="w-32 h-32 mb-4">
+        <div class="w-32 h-32 mx-auto md:my-12 my-6">
           <AmericanIcon></AmericanIcon>
         </div>
         
         {#if isSignIn}
           <form class="w-full m-auto justify-center items-center text-center" method="POST" action="?/login">
-            <input name="aadvantage" type="text" placeholder="AAdvantage Number" class="input input-xs w-full max-w-xs bg-base-200 text-neutral m-2" />
-            <input name="password" type="password" placeholder="Password" class="input input-xs w-full max-w-xs bg-base-200 text-neutral m-2" />
-            <input type="button" value="Log In" class="btn btn-primary btn-sm m-auto" formaction="?/login"/>
+            <input name="aadvantage" type="text" placeholder="AAdvantage Number" class="input input-sm w-full max-w-xs bg-base-200 text-neutral my-2" />
+            <input name="password" type="password" placeholder="Password" class="input input-sm w-full max-w-xs bg-base-200 text-neutral my-2" />
+            <input type="button" value="Log In" class="btn btn-primary btn-sm my-2" formaction="?/login"/>
           </form>
         {:else}
         <form class="w-full m-auto justify-center items-center text-center" method="POST" action="?/search">
-          <input name="search" type="text" placeholder="Search" class="input input-xs w-full max-w-xs bg-base-200 text-neutral m-2" />
-          <input type="button" value="Search" class="btn btn-primary btn-sm m-auto" />
+          <input name="search" type="text" placeholder="Search" class="input input-sm w-full max-w-xs bg-base-200 text-neutral my-2" />
+          <input type="button" value="Search" class="btn btn-primary btn-sm my-2" />
         </form>
         {/if}
       </div>
