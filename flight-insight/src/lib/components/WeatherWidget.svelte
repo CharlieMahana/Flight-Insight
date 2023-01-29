1911bc16cd0272a50e7f8e190e7727fc9536d9e9
Spaceface16518 <amrit.rathie@gmail.com>
2023-01-29 08:10:31 -0600

    extract into weather widget

---
+++ flight-insight/src/lib/components/WeatherWidget.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+  export let weather: {
+    time: string;
+    temperature: number;
+    precipitation: number;
+    cloudcover: number;
+  };
+  export let location: string;
+</script>
+
+<article
+  class="p-5 flex flex-col items-center justify-center gap-1 w-min bg-base-200 rounded-lg"
+>
+  <div class="flex flex-row justify-center p-2">
+    <div class="flex flex-col justify-center items-center">
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-16 h-16"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+          />
+        </svg>
+      </div>
+    </div>
+    <div class="text-5xl p-2">
+      {weather.temperature}&deg;F
+    </div>
+  </div>
+  <div>
+    <div>
+      <span class="text-2xl">{location}</span>
+    </div>
+  </div>
+  <div
+    class="flex flex-row justify-evenly border-t-base-300 border-solid w-full p-2"
+  >
+    <div class="flex flex-col justify-center">
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-9 h-9"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M2.25 15a4.5 4.5 0 004.5 4.5H18a3.75 3.75 0 001.332-7.257 3 3 0 00-3.758-3.848 5.25 5.25 0 00-10.233 2.33A4.502 4.502 0 002.25 15z"
+          />
+        </svg>
+      </div>
+      <div>
+        {weather.cloudcover * 100}%
+      </div>
+    </div>
+    <div>
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-9 h-9"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
+          />
+        </svg>
+      </div>
+      <div>
+        {weather.precipitation} in.
+      </div>
+    </div>
+  </div>
+</article>
--- flight-insight/src/routes/dashboard/+page.svelte
+++ flight-insight/src/routes/dashboard/+page.svelte
@@ -8,6 +8,7 @@
     import type { PageData } from "../$types";
     import ReportForm from "$lib/components/ReportForm.svelte";
     import { page } from "$app/stores";
+    import WeatherWidget from "$lib/components/WeatherWidget.svelte";
     mapboxgl.accessToken =
         "pk.eyJ1IjoiY2hhcmxpZW1haGFuYSIsImEiOiJja3FicmNhOWQwZDQwMnVvZW5pd3BnNGc4In0._TBwk5GaE5qqih2pilaLNw"; // default public access token
 
@@ -135,7 +136,7 @@
         src="https://api.mapbox.com/mapbox-assembly/v0.23.2/assembly.js"
     ></script>
 </svelte:head>
-<div>
+<div class="min-h-screen">
     <div class="w-full bg-primary h-16 flex ">
         <div class="flex flex-row align-middle text-center ">
             <div class="w-12 h-12 mr-4 m-auto ml-4">
@@ -154,9 +155,14 @@
         </div>
     {/if}
 
-    <div>
-        <div>
-            <div id="map" class="w-3/4 h-64 md:max-h-[500] " />
+    <div
+        class="flex flex-row-reverse flex-wrap justify-evenly items-center h-full p-6"
+    >
+        <div class="w-full lg:max-w-[50vw]">
+            <div
+                id="map"
+                class="w-full h-64 md:min-h-[80vh] md:h-full  rounded-md place-self-center"
+            />
             <style>
                 .marker {
                     background-image: url("https://www.iconpacks.net/icons/2/free-airport-location-icon-2959-thumb.png");
@@ -168,73 +174,55 @@
                     top: -20px;
                 }
             </style>
-            <div id="map" class="w-3/4 h-64 flex flex-col md:flex-row">
-                <div class="card bg-base-200 w-full h-96 m-2 p-4">
-                    <p class="text-xl">Departure Weather</p>
-                    <p>{data.originWeather.temperature}&deg;</p>
-                    <div class="w-64 h-64">
-                        {#if data.originWeather.precipitation > 0}
-                            <img src="/weather/rainy-2.svg" alt="rainy" />
-                        {:else if data.originWeather.cloudcover > 50}
-                            <img
-                                src="/weather/cloudy-day-2.svg"
-                                alt="cloudy day"
-                            />
-                        {:else}
-                            <img src="/weather/day.svg" alt="clear" />
-                        {/if}
-                    </div>
-                </div>
-                <div class="card bg-base-200 w-full h-96 m-2 p-4">
-                    <p class="text-xl">Arrival Weather</p>
-                    <p>{data.destinationWeather.temperature}&deg;</p>
-                </div>
-            </div>
+        </div>
 
-            <style>
-                #map {
-                    height: 500px;
-                    max-height: 500px;
-                    border-radius: 10px;
-                    margin: 20px auto;
-                }
-            </style>
+        <div
+            class="flex flex-row flex-wrap justify-evenly items-center gap-3 p-6"
+        >
+            <WeatherWidget
+                weather={data.originWeather}
+                location={data.flight.origin.city}
+            />
+            <WeatherWidget
+                weather={data.destinationWeather}
+                location={data.flight.destination.city}
+            />
         </div>
     </div>
-
-    <label
-        for="report-form-modal"
-        class="btn fixed bottom-1 right-1 md:bottom-4 md:right-4"
-    >
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
-        >
-            <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M10.05 4.575a1.575 1.575 0 10-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 013.15 0v1.5m-3.15 0l.075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 013.15 0V15M6.9 7.575a1.575 1.575 0 10-3.15 0v8.175a6.75 6.75 0 006.75 6.75h2.018a5.25 5.25 0 003.712-1.538l1.732-1.732a5.25 5.25 0 001.538-3.712l.003-2.024a.668.668 0 01.198-.471 1.575 1.575 0 10-2.228-2.228 3.818 3.818 0 00-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0116.35 15m.002 0h-.002"
-            />
-        </svg>
-    </label>
-
-    <input type="checkbox" id="report-form-modal" class="modal-toggle" />
-    <label
-        for="report-form-modal"
-        class="modal modal-bottom sm:modal-middle cursor-pointer"
-    >
-        <label class="modal-box relative" for="">
-            <ReportForm
-                seat={form?.seat}
-                complaint={form?.complaint}
-                flightnum={form?.flightnum}
-                success={form?.success}
-                missing={form?.missing}
-            />
-        </label>
-    </label>
 </div>
+
+<label
+    for="report-form-modal"
+    class="btn fixed bottom-1 right-1 md:bottom-4 md:right-4"
+>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6"
+    >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M10.05 4.575a1.575 1.575 0 10-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 013.15 0v1.5m-3.15 0l.075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 013.15 0V15M6.9 7.575a1.575 1.575 0 10-3.15 0v8.175a6.75 6.75 0 006.75 6.75h2.018a5.25 5.25 0 003.712-1.538l1.732-1.732a5.25 5.25 0 001.538-3.712l.003-2.024a.668.668 0 01.198-.471 1.575 1.575 0 10-2.228-2.228 3.818 3.818 0 00-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0116.35 15m.002 0h-.002"
+        />
+    </svg>
+</label>
+
+<input type="checkbox" id="report-form-modal" class="modal-toggle" />
+<label
+    for="report-form-modal"
+    class="modal modal-bottom sm:modal-middle cursor-pointer"
+>
+    <label class="modal-box relative" for="">
+        <ReportForm
+            seat={form?.seat}
+            complaint={form?.complaint}
+            flightnum={form?.flightnum}
+            success={form?.success}
+            missing={form?.missing}
+        />
+    </label>
+</label>
