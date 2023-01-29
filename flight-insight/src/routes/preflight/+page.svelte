814f04a6a77edc77e596516fc927fd69a9d0f82b
heartgg <a.szumski98@gmail.com>
2023-01-29 12:10:18 -0600

    font

--- flight-insight/src/routes/preflight/+page.svelte
+++ flight-insight/src/routes/preflight/+page.svelte
@@ -23,12 +23,12 @@
             <div class="carousel-item w-full justify-center items-center bg-neutral-50">
                 <div class="columns-1 p-10 h-144 text-center justify-center items-center my-8">
                     <h1 class="text-9xl">❄</h1>
-                    <h1 class="text-4xl">The cabin temperature is</h1>
-                    <h1 class="md:text-9xl text-8xl my-6">{temp}°F</h1>
+                    <h1 class="text-3xl">The cabin temperature is</h1>
+                    <h1 class="md:text-8xl text-6xl my-6">{temp}°F</h1>
                     {#if temp <= 68}
-                        <h1 class="text-4xl">Consider bringing a <b>jacket</b> on-board.</h1>
+                        <h1 class="text-3xl">Consider bringing a <b>jacket</b> on-board.</h1>
                     {:else}
-                        <h1 class="text-4xl">You may not need a jacket on-board.</h1>
+                        <h1 class="text-3xl">You may not need a jacket on-board.</h1>
                     {/if}
                 </div>
               <!-- <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" class="w-full" alt="Tailwind CSS Carousel component" /> -->
@@ -36,24 +36,24 @@
             <div class="carousel-item w-full bg-neutral-50">
                 <div class="columns-1 p-10 h-144 text-center justify-center items-center my-8">
                     <h1 class="text-8xl">✈︎</h1>
-                    <h1 class="text-4xl">The chance of severe turbulence is</h1>
+                    <h1 class="text-3xl">The chance of severe turbulence is</h1>
                     <div class="radial-progress my-6" style="--value:{turbulence}; --size:8rem; --thickness: 1rem;">{turbulence}%</div>
                     {#if turbulence >= 50}
-                        <h1 class="text-4xl">Consider <b>upgrading your seats</b> and bringing <b>motion sickness</b> medication.</h1>
+                        <h1 class="text-3xl">Consider <b>upgrading your seats</b> and bringing <b>motion sickness</b> medication.</h1>
                     {:else}
-                        <h1 class="text-4xl">If you are prone to motion sickness, consider <b>preventative care</b>.</h1>
+                        <h1 class="text-3xl">If you are prone to motion sickness, consider <b>preventative care</b>.</h1>
                     {/if}
                 </div>
             </div> 
             <div class="carousel-item w-full bg-neutral-50">
                 <div class="columns-1 p-10 h-144 text-center justify-center items-center my-8">
                     <h1 class="text-8xl">☁</h1>
-                    <h1 class="text-4xl">The cabin humidity is</h1>
+                    <h1 class="text-3xl">The cabin humidity is</h1>
                     <div class="radial-progress my-6" style="--value:{humidity}; --size:8rem; --thickness: 1rem;">{humidity}%</div>
                     {#if humidity <= 40}
-                        <h1 class="text-4xl">Consider bringing TSA-approved <b>lotion</b> and <b>eye drops</b>.</h1>
+                        <h1 class="text-3xl">Consider bringing TSA-approved <b>lotion</b> and <b>eye drops</b>.</h1>
                     {:else}
-                        <h1 class="text-4xl">If you are prone to low humidity effects, consider <b>preventative care</b>.</h1>
+                        <h1 class="text-3xl">If you are prone to low humidity effects, consider <b>preventative care</b>.</h1>
                     {/if}
                 </div>
               <!-- <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" class="w-full" alt="Tailwind CSS Carousel component" /> -->
