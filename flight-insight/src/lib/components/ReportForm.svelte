ead81e5c724bea9d926b0e53f2113fb45d3cf357
Spaceface16518 <amrit.rathie@gmail.com>
2023-01-29 04:47:17 -0600

    integrate report form

--- flight-insight/src/lib/components/ReportForm.svelte
+++ flight-insight/src/lib/components/ReportForm.svelte
@@ -1,12 +1,24 @@
-<form class="form-control mb-3 p-2">
+<script lang="ts">
+  export let seat: string;
+  export let complaint: string;
+  export let success = false;
+  export let missing = false;
+</script>
+
+<form class="form-control mb-2 p-2" method="post">
   <h2 class="text-2xl font-bold mb-8">Contact a Flight Attendant</h2>
-  <label class="mb-10 input-group">
+  <label class="mb-3 input-group">
     <span>Seat</span>
-    <input class="input bg-base-200" placeholder="69A" name="seat" />
+    <input
+      class="input bg-base-200"
+      placeholder="69A"
+      name="seat"
+      bind:value={seat}
+    />
   </label>
-  <label class="mb-10 input-group">
+  <label class="mb-3 input-group">
     <span>Report</span>
-    <select name="complaint" class="p-3 w-full">
+    <select name="complaint" class="p-3 w-full" bind:value={complaint}>
       <option value="food">Request Food</option>
       <option value="drink">Request Drink</option>
       <option value="blanket">Request Blanket</option>
@@ -16,5 +28,20 @@
       <option value="other">Call Flight Attendant</option>
     </select>
   </label>
-  <button class="btn btn-primary" type="submit">Submit</button>
+  <button class="btn btn-primary mb-3" type="submit">Submit</button>
+
+  {#if success}
+    <div class="mb-2">
+      <div class="alert alert-success shadow-lg">
+        <div>Request sent!</div>
+      </div>
+    </div>
+  {/if}
+  {#if missing}
+    <div class="mb-2">
+      <div class="alert alert-error shadow-lg">
+        <div>Missing seat or request</div>
+      </div>
+    </div>
+  {/if}
 </form>
